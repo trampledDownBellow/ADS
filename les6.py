a97cfa531840bcb1662fc7d7ee42cbc3f94bbd66
Stas <sodotarsouron@gmail.com>
2023-11-09 12:41:05 +0200

    upd

--- les6.py
+++ les6.py
@@ -1,22 +1,22 @@
-from collections import deque              
+from collections import deque              # Двостороння черга
 
 def dfs(graph, start):
-    visited = set()                       
-    stack = [start]                            
+    visited = set()                         # Створюємо пустий набір для відвіданих вузлів
+    stack = [start]                            # Створюємо стек і додаємо початковий вузол до стеку
 
     while stack:
-        vertex = stack.pop()                
+        vertex = stack.pop()                # Беремо вершину з вершини стеку
         if vertex not in visited:
             print(vertex)
             visited.add(vertex)
             stack.extend(neighbor for neighbor in graph[vertex] if neighbor not in visited)
 
 def bfs(graph, start):
-    visited = set()                         
-    queue = deque([start])                   
+    visited = set()                         # Створюємо пустий набір для відвіданих вузлів
+    queue = deque([start])                   # Створюємо чергу і додаємо початковий вузол до черги
 
     while queue:
-        vertex = queue.popleft()             
+        vertex = queue.popleft()             # Беремо вершину з початку черги
         if vertex not in visited:
             print(vertex)
             visited.add(vertex)
@@ -31,10 +31,10 @@ graph = {
     'F': ['D', 'C',]
 }
 
-print("DFS:")
+print("\nDFS:")
 dfs(graph, 'A')
 
-print("BFS:")
+print("\nBFS:")
 bfs(graph, 'A')
 
 graph = {
@@ -46,9 +46,24 @@ graph = {
     'F': []
 }
 
-print("DFS:")
+print("\nDFS:")
 dfs(graph, 'A')
 
-print("BFS:")
+print("\nBFS:")
+bfs(graph, 'A')
+
+graph = {
+    'A': ['3', '9'],
+    'B': ['2', '5',],
+    'C': ['12', '9', '4',],
+    'D': ['4'],
+    'E': [],
+    'F': []
+}
+
+print("\nDFS:")
+dfs(graph, 'A')
+
+print("\nBFS:")
 bfs(graph, 'A')
 
